da6904653beeede30b16beb976b99be7df24cd18
Jacob Anderson <andersonjwan@gmail.com>
2022-03-29 13:20:29 -0700

    refactor: Move template definitions to hpp file

--- src/operators/crossover.hpp
+++ src/operators/crossover.hpp
@@ -1,9 +1,12 @@
 #ifndef GENALG_OPERATORS_CROSSOVER_HPP
 #define GENALG_OPERATORS_CROSSOVER_HPP
 
+#include <algorithm>
 #include <array>
 #include <cassert>
 #include <cstddef>
+#include <random>
+#include <utility>
 #include <vector>
 
 namespace genalg {
@@ -34,5 +37,59 @@ namespace genalg {
     }
 }
 
-#include "crossover.tpp"
+namespace genalg {
+    namespace operators {
+	template<typename T>
+	std::array<T, 2> SinglePointCrossover<T>::crossover(const T& p1, const T& p2) {
+	    assert(p1.size() == p2.size());
+
+	    std::random_device rd;
+	    std::mt19937 gen(rd());
+	    std::uniform_int_distribution<int> distr(1, p1.size());
+
+	    int index = distr(gen);
+
+	    T child_genome1(p1);
+	    T child_genome2(p2);
+
+	    for(int i = index; i < child_genome1.size(); ++i) {
+		std::swap(child_genome1[i], child_genome2[i]);
+	    }
+
+	    return std::array<T, 2> {child_genome1, child_genome2};
+	}
+
+	template<typename T>
+	std::array<T, 2> MultiPointCrossover<T>::crossover(const T& p1, const T& p2) {
+	    assert(p1.size() == p2.size());
+	    assert(this->n_points <= p1.size());
+
+	    std::random_device rd;
+	    std::mt19937 gen(rd());
+
+	    std::vector<std::size_t> indices(p1.size() - 1);
+	    std::iota(indices.begin(), indices.end(), 1);
+
+	    std::shuffle(indices.begin(), indices.end(), gen);
+
+	    std::vector<std::size_t> points(this->n_points);
+	    for(int i = 0; i < this->n_points; ++i) {
+		points[i] = indices[i];
+	    }
+
+	    T child_genome1(p1);
+	    T child_genome2(p2);
+
+	    std::sort(points.begin(), points.end());
+	    for(int i = 0; i < points.size(); ++i) {
+		for(int j = points[i]; j < child_genome1.size(); ++j) {
+		    std::swap(child_genome1[j], child_genome2[j]);
+		}
+	    }
+
+	    return std::array<T, 2> {child_genome1, child_genome2};
+	}
+    }
+}
+
 #endif
--- src/operators/selection.hpp
+++ src/operators/selection.hpp
@@ -1,8 +1,18 @@
 #ifndef GENALG_OPERATORS_SELECTION_HPP
 #define GENALG_OPERATORS_SELECTION_HPP
 
+#include <algorithm>
+#include <array>
 #include <cstddef>
+#include <iterator>
 #include <map>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <stdexcept>
+#include <utility>
+#include <vector>
 
 namespace genalg {
     namespace operators {
@@ -12,18 +22,20 @@ namespace genalg {
 	    virtual T select(std::map<T, F>) = 0;
 	};
 
-	template<typename T, typename F>
+	template<typename R, typename T, typename F>
 	class TournamentSelection : public SelectionOperator<T, F>{
 	private:
+	    R rng;
+
 	    std::size_t tournament_size;
 	    double p = 0.5;
 
 	public:
-	    TournamentSelection<T, F>(std::size_t size)
-		: tournament_size {size} {}
+	    TournamentSelection<R, T, F>(R r, std::size_t size)
+		: rng{r}, tournament_size{size} {}
 
-	    TournamentSelection<T, F>(std::size_t size, double prob)
-		: tournament_size {size}, p {prob} {}
+	    TournamentSelection<R, T, F>(R r, std::size_t size, double prob)
+		: rng{r}, tournament_size{size}, p{prob} {}
 
 	    // virtual T select(std::map<T, F>) override;
 	    T select(std::map<T, F>);
@@ -31,5 +43,51 @@ namespace genalg {
     }
 }
 
-#include "operators/selection.tpp"
+namespace genalg {
+    namespace operators {
+	template<typename R, typename T, typename F>
+	T TournamentSelection<R, T, F>::select(std::map<T, F> population) {
+	    if(this->tournament_size > population.size()) {
+		throw std::out_of_range("tournament size must be less than or equal to population size");
+	    }
+
+	    using competitor = std::pair<T, F>;
+
+	    std::vector<std::size_t> indices(population.size());
+	    std::iota(indices.begin(), indices.end(), 0);
+
+	    std::shuffle(indices.begin(), indices.end(), rng);
+
+	    // select individuals to compete
+	    auto cmp = [](competitor c1, competitor c2) { return c1.second < c2.second; };
+	    std::priority_queue<competitor, std::vector<competitor>, decltype(cmp)> selections(cmp);
+
+	    auto iter_begin = population.begin();
+	    for(std::size_t i = 0; i < this->tournament_size; ++i) {
+		auto iter = population.begin();
+		std::advance(iter, indices[i]);
+
+		auto sel = iter;
+		selections.push(std::make_pair(sel->first, sel->second));
+	    }
+
+	    // select top two competitors
+	    T c1 = (selections.top()).first;
+	    selections.pop();
+	    T c2 = (selections.top()).first;
+	    selections.pop();
+
+	    std::array<T, 2> matchup = {c1, c2};
+
+	    std::uniform_real_distribution<double> distr(0.0, 1.0);
+
+	    if(distr(rng) <= this->p) {
+		return matchup[0]; // return most fit
+	    } else {
+		return matchup[1]; // return second-most fit
+	    }
+	}
+    }
+}
+
 #endif
