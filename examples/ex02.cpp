ea410e4276c98bd2f747b5d923b2c180227ad92e
Jacob Anderson <andersonjwan@gmail.com>
2022-03-30 00:11:16 -0700

    examples: Add population usage example

---
+++ examples/ex02.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "population.hpp"
+#include "population/individual.hpp"
+
+using genalg::population::Individual;
+using genalg::Population;
+
+using genome = double;
+using fitness = double;
+
+#define POPULATION_SIZE 25
+
+// custom individual
+template<typename G, typename F>
+class Species : public Individual<G, F> {
+public:
+    Species(G g)
+	: Individual<G, F>(g) {}
+
+    F fitness(void) const override {
+	return std::sin(this->genome) - 0.2 * std::abs(this->genome);
+    }
+};
+
+int
+main(int argc, char **argv) {
+    Population<Species<genome, fitness>, fitness> population(POPULATION_SIZE);
+
+    std::mt19937* rng;
+    if(argc == 2) {
+	// use provided SEED
+	rng = new std::mt19937(std::stoi(argv[1]));
+    } else {
+	std::random_device rd;
+	rng = new std::mt19937(rd());
+    }
+
+    std::uniform_real_distribution<double> uniform(-10.0, 10.0);
+
+    // randomly initialize population
+    for(int i = 0; i < POPULATION_SIZE; ++i) {
+	population.add(Species<genome, fitness>(uniform(*rng)));
+    }
+
+    for(int i = 0; i < POPULATION_SIZE; ++i) {
+	std::cout << "{"
+		  << population[i].first.get_genome()
+		  << " , "
+		  << population[i].second
+		  << "}\n";
+    }
+
+    delete rng;
+}
