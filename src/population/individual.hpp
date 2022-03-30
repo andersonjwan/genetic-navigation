a699b017da8f394935abdea0237215beed697677
Jacob Anderson <andersonjwan@gmail.com>
2022-03-29 23:17:46 -0700

    feat: Implement individual base class

---
+++ src/population/individual.hpp
@@ -0,0 +1,31 @@
+#ifndef GENALG_POPULATION_INDIVIDUAL_HPP
+#define GENALG_POPULATION_INDIVIDUAL_HPP
+
+#include <cstddef>
+
+namespace genalg {
+    namespace population {
+	template<typename G, typename F>
+	class Individual {
+	protected:
+	    G genome;
+	    std::size_t age = 0;
+
+	public:
+	    Individual(G g)
+		: genome{g} {}
+
+	    G get_genome(void);
+	    virtual F fitness(void) const = 0;
+	};
+    }
+}
+
+namespace genalg {
+    namespace population {
+	template<typename G, typename F>
+	G Individual<G, F>::get_genome(void) { return this->genome; }
+    }
+}
+
+#endif
