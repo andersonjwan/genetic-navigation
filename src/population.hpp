017021a4df7c0121ca2ffdb210e39c3e928d90ef
Jacob Anderson <andersonjwan@gmail.com>
2022-03-30 00:54:14 -0700

    feat: Implement best/worst solution filters

--- src/population.hpp
+++ src/population.hpp
@@ -1,6 +1,7 @@
 #ifndef GENALG_POPULATION_HPP
 #define GENALG_POPULATION_HPP
 
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
 #include <utility>
@@ -16,13 +17,22 @@ namespace genalg {
 	std::vector<std::pair<I, F>> individuals;
 
     public:
-	Population(std::size_t s)
+	explicit Population(std::size_t s)
 	    : size{s} {}
 
 	void add(const I& individual);
 
+	// analytics
+	const std::pair<I, F>& best(void) const;
+	const std::pair<I, F>& worst(void) const;
+
 	// operations
 	const std::pair<I, F>& operator[](int i) const;
+
+	// custom comparators
+	static
+	bool _cmp_individuals(const std::pair<I, F>& lhs,
+			      const std::pair<I, F>& rhs);
     };
 }
 
@@ -37,9 +47,29 @@ namespace genalg {
     }
 
     template<typename I, typename F>
+    const std::pair<I, F>& Population<I, F>::best() const {
+	return *std::max_element(this->individuals.begin(),
+				 this->individuals.end(),
+				 Population<I, F>::_cmp_individuals);
+    }
+
+    template<typename I, typename F>
+    const std::pair<I, F>& Population<I, F>::worst() const {
+	return *std::min_element(this->individuals.begin(),
+				 this->individuals.end(),
+				 Population<I, F>::_cmp_individuals);
+    }
+
+    template<typename I, typename F>
     const std::pair<I, F>& Population<I, F>::operator[](int i) const {
 	assert(i >= 0);
 	return individuals[i];
+    }
+
+    template<typename I, typename F>
+    bool Population<I, F>::_cmp_individuals(const std::pair<I, F>& lhs,
+						  const std::pair<I, F>& rhs) {
+	return lhs.second < rhs.second;
     }
 }
 
