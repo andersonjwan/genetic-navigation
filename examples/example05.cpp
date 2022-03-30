e1df811c9223bcd8827708019a45bde800a7cfea
Jacob Anderson <andersonjwan@gmail.com>
2022-03-29 21:29:29 -0700

    examples: Add custom individual example

---
+++ examples/example05.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include "population/individual.hpp"
+
+#define POPULATION_SIZE 25
+
+using genalg::population::Individual;
+
+using genome = double;
+using metric = double;
+
+template<typename T, typename F>
+class MyIndividual : public Individual<T, F> {
+public:
+    MyIndividual(T g)
+	: Individual<T, F>(g) {}
+
+    F fitness() override {
+	return std::abs(this->genome) * this->genome;
+    }
+};
+
+int
+main() {
+    std::vector<MyIndividual<genome, metric>> population;
+
+    for(int i = 0; i < POPULATION_SIZE; ++i) {
+	population.push_back(MyIndividual<genome, metric>(i));
+	std::cout << "{" << i << " , " << population[i].fitness() << "}\n";
+    }
+}
