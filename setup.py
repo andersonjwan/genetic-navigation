dbea52bb89496a5207e2ba6eed64552b7cf02fe2
Jacob Anderson <andersonjwan@gmail.com>
2022-04-09 13:24:58 -0700

    build: Implement simple cythonize

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+extensions = [
+    Extension(
+        name="genalgplusplus",
+        sources=["bindings/optionsplusplus.pyx"],
+        include_dirs=["src"]
+    )
+]
+
+setup(
+    name="py-genalg",
+    version="0.0.1",
+    ext_package="pygenalg",
+    ext_modules=cythonize(extensions),
+    python_requires=">=3.8"
+)
