bd17456a121944a4c4a5db9540218b96619aeeb8
Jacob Anderson <andersonjwan@gmail.com>
2022-04-09 19:17:51 -0700

    refactor: Update key arguments

--- examples/python/objective_fn.py
+++ examples/python/objective_fn.py
@@ -2,9 +2,9 @@ from pygenalg.genalgplusplus import Options
 
 if __name__ == "__main__":
     options = Options(
-        population_size=50,
+        capacity=50,
         n_generations=100,
-        mutation_chance=0.01
+        p_mutation=0.01
     )
 
     print(options.seed)
