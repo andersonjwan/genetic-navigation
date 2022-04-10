244ba6a7569e97ab524dc22d9e31becd16317f16
Jacob Anderson <andersonjwan@gmail.com>
2022-04-09 23:11:29 -0700

    refactor: Update to match new package interface

--- examples/python/objective_fn.py
+++ examples/python/objective_fn.py
@@ -1,9 +1,17 @@
-from pygenalg.algorithmplusplus import GeneticAlgorithm
+from pygenalg.algorithm import GeneticAlgorithm
+from pygenalg.options import Options
 
 if __name__ == "__main__":
     ga = GeneticAlgorithm(
         capacity=50,
         n_generations=100,
-        p_mutation=0.01,
-        seed=12345
+        p_mutation=0.01
     )
+
+    options = Options(
+        capacity=50,
+        n_generations=100,
+        p_mutation=0.01
+    )
+
+    print(options.seed)
