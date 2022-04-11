cf7b37917a6489fc61836b3e989dea196163b18a
Jacob Anderson <andersonjwan@gmail.com>
2022-04-10 22:57:57 -0700

    feat: Implement individual interfaces

--- bindings/individual.pyx
+++ bindings/individual.pyx
@@ -3,16 +3,32 @@
 from individual_cpp cimport BinaryIndividual as cppBinaryIndividual
 
 cdef class BinaryIndividual:
+    """A solution encoded as a string of 1's or 0's.
+    """
+
     cdef cppBinaryIndividual* cpp_individual
 
     def __cinit__(self, genome):
-        """
+        """Allocate a new BinaryIndividual (C++).
         """
 
         self.cpp_individual = new cppBinaryIndividual(genome)
 
-    def __dealloc__(self):
+
+    def fitness(self):
+        """Compute the fitness of the individual.
         """
+
+        return self.cpp_individual.fitness()
+
+
+    @property
+    def genome(self):
+        return self.cpp_individual.get_genome()
+
+
+    def __dealloc__(self):
+        """Deallocate BinaryIndividual (C++).
         """
 
         del self.cpp_individual
--- bindings/individual_cpp.pxd
+++ bindings/individual_cpp.pxd
@@ -4,3 +4,7 @@ from libcpp.vector cimport vector
 cdef extern from "population/individual.hpp" namespace "genalg::population":
     cdef cppclass BinaryIndividual:
         BinaryIndividual(vector[bool] genome)
+
+        # methods
+        vector[bool] get_genome() const
+        double fitness() const
