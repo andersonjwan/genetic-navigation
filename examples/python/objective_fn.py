638b0ee2fa1e6473dd1133e7821310601a3875c6
Jacob Anderson <andersonjwan@gmail.com>
2022-04-11 20:59:40 -0700

    examples: Add complete py-GA example

--- examples/python/objective_fn.py
+++ examples/python/objective_fn.py
@@ -1,17 +1,38 @@
 from pygenalg.algorithm import GeneticAlgorithm
 from pygenalg.options import Options
+from pygenalg.population import Population
+from pygenalg.individual import BinaryIndividual
+
+def rank(population: Population):
+    """Adjust individual fitness based on combination.
+    """
+    adjusted = Population(len(population))
+
+    for individual in population:
+        adjusted.add(BinaryIndividual(individual[0]), individual[0].count(1))
+
+    return adjusted
 
 if __name__ == "__main__":
     ga = GeneticAlgorithm(
-        capacity=50,
+        capacity=4,
         n_generations=100,
         p_mutation=0.01
     )
 
-    options = Options(
-        capacity=50,
-        n_generations=100,
-        p_mutation=0.01
-    )
+    population = Population(4)
 
-    print(options.seed)
+    population.add(BinaryIndividual([1, 1, 1, 1, 0, 1, 0, 1, 0]), 3.0)
+    population.add(BinaryIndividual([1, 1, 1, 1, 0, 0, 0, 0, 0]), 2.0)
+    population.add(BinaryIndividual([1, 1, 1, 1, 1, 1, 1, 1, 1]), 1.0)
+    population.add(BinaryIndividual([0, 0, 0, 0, 0, 0, 0, 0, 0]), 0.0)
+
+    for i in range(100):
+        population = ga.update(population)
+
+        # update the fitness of each individual based on an external
+        # metric (e.g., environmental factors)
+        population = rank(population)
+
+        for individual in population:
+            print(f"({individual[0]}, {individual[1]})")
