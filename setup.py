db90aee6310c27eedb84aba942c5dcf99428d22d
Jacob Anderson <andersonjwan@gmail.com>
2022-04-12 19:26:30 -0700

    build: Add Python dependencies

--- setup.py
+++ setup.py
@@ -5,37 +5,37 @@ extensions = [
     Extension(
         name="algorithm",
         sources=["bindings/algorithm.pyx"],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="crossover",
         sources=["bindings/crossover.pyx"],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="mutation",
         sources=["bindings/mutation.pyx"],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="options",
         sources=["bindings/options.pyx",],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="population",
         sources=["bindings/population.pyx",],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="selection",
         sources=["bindings/selection.pyx",],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
     Extension(
         name="individual",
         sources=["bindings/individual.pyx"],
-        include_dirs=["src"]
+        include_dirs=["src/genalg"]
     ),
 ]
 
@@ -44,5 +44,11 @@ setup(
     version="0.0.1",
     ext_package="pygenalg",
     ext_modules=cythonize(extensions),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "pandas"
+    ],
     python_requires=">=3.8"
 )
