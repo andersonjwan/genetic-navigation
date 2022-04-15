140e03f87af259e1f973b13fbe8164b134c1bd83
Jacob Anderson <andersonjwan@gmail.com>
2022-04-14 23:14:25 -0700

    refactor: Add documentation and change declaration modifiers

--- src/genalg/algorithm/termination.hpp
+++ src/genalg/algorithm/termination.hpp
@@ -7,33 +7,52 @@
 
 namespace genalg {
     namespace algorithm {
+        /// Interface for a terminating the Genetic Algorithm (GA).
+        ///
+        /// @tparam I An individual
+        /// @tparam F the metric used to evaluate fitness
         template<typename I, typename F>
         class TerminationCondition {
         public:
-            virtual bool terminate(std::vector<std::pair<I, F>>& population) = 0;
+            virtual bool terminate(const std::vector<std::pair<I, F>>& population) = 0;
         };
 
+        /// Termination condition for GA based on the generation count.
+        ///
+        /// The GA will terminate after the defined generation limit is
+        /// reached.
+        ///
+        /// @tparam I An individual
+        /// @tparam F the metric used to evaluate fitness
         template<typename I, typename F>
         class GenerationLimit : public TerminationCondition<I, F> {
         private:
-            std::size_t limit;
-            std::size_t current = 0;
+            const std::size_t limit_;
+            std::size_t current_ = 0;
 
         public:
-            GenerationLimit(std::size_t l)
-                : limit{l} {}
+            explicit GenerationLimit(std::size_t l)
+                : limit_{l} {}
 
-            bool terminate(std::vector<std::pair<I, F>>& population) override;
+            bool terminate(const std::vector<std::pair<I, F>>& population) override;
         };
     }
 }
 
 namespace genalg {
     namespace algorithm {
+        /// Check if the generation limit has been reached.
+        ///
+        /// The \ref GenerationLimit keeps track of the total number
+        /// of generations and checks at each episode whether that limit
+        /// has been reached. If so, it returns True.
+        ///
+        /// @param population The latest \ref Population
+        /// @return To terminate or not
         template<typename I, typename F>
-        bool GenerationLimit<I, F>::terminate(std::vector<std::pair<I, F>>& population) {
-            this->current++;
-            return this->current >= this->limit;
+        bool GenerationLimit<I, F>::terminate(const std::vector<std::pair<I, F>>& population) {
+            this->current_++;
+            return this->current >= this->limit_;
         }
     }
 }
