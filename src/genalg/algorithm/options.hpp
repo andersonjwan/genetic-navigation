e738eb9d6233cb8293365d0eb0d6328489db35cc
Jacob Anderson <andersonjwan@gmail.com>
2022-04-14 22:59:29 -0700

    refactor: Add documentation and remove number of generations as an option The number of generations should be part of the termination interface

--- src/genalg/algorithm/options.hpp
+++ src/genalg/algorithm/options.hpp
@@ -6,24 +6,42 @@
 
 namespace genalg {
     namespace algorithm {
+        /// Set of options to provide to \ref GeneticAlgorithm interface.
+        ///
+        /// The population size and the chance of mutation must be defined
+        /// for the options. If no seed is provided, a randomly generated seed
+        /// will be set.
         class Options {
         public:
-            const std::size_t population_size;
-            const std::size_t n_generations;
-            const double mutation_chance = 0.01;
+            /// The number of individuals per \ref Population
+            const std::size_t population_capacity;
+
+            /// The probability of a mutation occurring
+            const double p_mutation = 0.01;
+
+            /// A optional value to seed the Random Number Generator (RNG) engine
             unsigned long int seed;
 
-            Options(std::size_t p, std::size_t g, double m)
-                : population_size{p}, n_generations{g}, mutation_chance{m} {
+            /// Initialize the options with a randomly generated seed.
+            ///
+            /// @param p The \ref Population capacity
+            /// @param m The probability of a mutation occurring in an offspring
+            Options(std::size_t p, double m)
+                : population_capacity{p}, p_mutation{m} {
+                // randomly generate a seed
                 std::random_device rd;
                 std::uniform_int_distribution<unsigned long int> distr(0, std::pow(2, 32) - 1);
 
                 seed = distr(rd);
             }
 
-            Options(std::size_t p, std::size_t g, double m, unsigned long int s)
-                : population_size{p}, n_generations{g}, mutation_chance{m},
-                  seed{s} {}
+            /// Initialize the options with a provided seed.
+            ///
+            /// @param p The \ref Population capacity
+            /// @param m The probability of a mutation occurring in an offspring
+            /// @param s A number to seed the program.
+            Options(std::size_t p, double m, unsigned long int s)
+                : population_capacity{p}, p_mutation{m}, seed{s} {}
 
         };
     }
