66dc7fb8d587e7ac77c971d18e41a79874fb6594
Jacob Anderson <andersonjwan@gmail.com>
2022-04-17 02:31:46 -0700

    refactor: Simplify selection operation

--- src/genalg/operators/selection.hpp
+++ src/genalg/operators/selection.hpp
@@ -5,90 +5,82 @@
 #include <cassert>
 #include <cstddef>
 #include <random>
-#include <utility>
 #include <vector>
+
+#include "population.hpp"
 
 namespace genalg {
     namespace operators {
-        /// Interface for GA selection operations.
+        /// Interface for Genetic Algorithm selection operations.
         ///
         /// @tparam I An individual
-        /// @tparam F The metric used to evaluate the fitness of an individual
-        template<typename I, typename F>
+        template<typename I>
         class SelectionOperator {
         public:
-            virtual I select(const std::vector<std::pair<I, F>>& population,
-                             std::default_random_engine& rng) const = 0;
+            virtual I select(const Population<I>& population,
+                                    std::default_random_engine& rng) const = 0;
         };
 
-        /// Selection using tournament-style operations.
+        /// Selection using a tournament selection process.
         ///
-        /// The tournament selection process involves a tournament size
-        /// (i.e., how many individuals to compete), the chance of selecting
-        /// the best fit, and whether that selected individual gets replaced
-        /// to be picked again.
+        /// Selection through tournament selection involves including
+        /// a specified amount of individuals to compete and selecting
+        /// the most fit individual with a defined probability.
         ///
         /// @tparam I An individual
-        /// @tparam F The metric used to evaluate the fitness of an individual
-        template<typename I, typename F>
-        class TournamentSelection : public SelectionOperator<I, F> {
+        template<typename I>
+        class TournamentSelection : public SelectionOperator<I> {
         private:
-            const std::size_t tournament_size_;
-            const double prob_;
-            const bool replacement_;
+            const std::size_t pool_size_;
+            const double p_fittest_;
 
         public:
-            TournamentSelection(std::size_t s=2, double p=0.5, bool rep=true)
-                : tournament_size_{s}, prob_{p}, replacement_{rep} {}
+            explicit TournamentSelection(std::size_t size=2, double prob=0.5)
+                : pool_size_{size}, p_fittest_{prob} {
+                assert(this->pool_size_ > 0);
+            }
 
-            I select(const std::vector<std::pair<I, F>>& population,
-                     std::default_random_engine& rng) const override;
+            I select(const Population<I>& population,
+                            std::default_random_engine& rng) const override;
         };
     }
 }
 
 namespace genalg {
     namespace operators {
-        /// Select the tournament size of individuals to compete.
+        /// Select an individual.
         ///
-        /// @param population The population fitness list
+        /// @param population A \ref Population to select from
         /// @param rng The Random Number Generator engine
-        /// @return The selected individual (i.e, the winner)
-        template<typename I, typename F>
-        I TournamentSelection<I, F>::select(const std::vector<std::pair<I, F>>& population,
-                                            std::default_random_engine& rng) const {
-            assert(population.size() >= 1);
-            assert(this->tournament_size_ <= population.size());
+        /// @return An individual
+        template<typename I>
+        I TournamentSelection<I>::select(const Population<I>& population,
+                                                std::default_random_engine& rng) const {
+            assert(population.size() > 0);
+            assert(this->pool_size_ <= population.size());
 
             // randomly select individuals
-            std::vector<std::pair<I, F>> pool;
+            std::vector<I> pool;
 
-            if(this->replacement_) {
-                for(int i = 0; i < this->tournament_size_; ++i) {
-                    std::sample(population.begin(), population.end(),
-                                std::back_inserter(pool), 1, rng);
-                }
-            } else {
-                std::sample(population.begin(), population.end(),
-                            std::back_inserter(pool), this->tournament_size_, rng);
-            }
+            std::sample(population.begin(), population.end(),
+                        std::back_inserter(pool), this->pool_size_, rng);
 
-            // find best from pool
-            auto winner = std::max_element(pool.begin(), pool.end(),
-                                           [](const auto& lhs, const auto& rhs) {
-                                               return lhs.second < rhs.second;
-                                           });
+            auto fittest = std::max_element(pool.begin(), pool.end());
 
-            // select best with probability p
             std::uniform_real_distribution<double> rdistr(0.0, 1.0);
-            if(rdistr(rng) <= this->prob_) {
-                return (*winner).first;
+            if(rdistr(rng) < this->p_fittest_) {
+                return *fittest;
             } else {
-                pool.erase(winner);
-                std::sample(pool.begin(), pool.end(),
-                            std::back_inserter(pool), 1, rng);
+                std::vector<I> unfit;
+                std::copy_if(pool.begin(), pool.end(), std::back_inserter(unfit),
+                             [fittest](const auto& x) {
+                                 return x != *fittest;
+                             });
 
-                return pool.back().first;
+                std::sample(unfit.begin(), unfit.end(),
+                            std::back_inserter(unfit), 1, rng);
+
+                return unfit.back();
             }
         }
     }
