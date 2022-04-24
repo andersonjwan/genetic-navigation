d192756e6e3cc92f871d2f54b3a550cd7609e6b3
Jacob Anderson <andersonjwan@gmail.com>
2022-04-24 15:11:25 -0700

    feat: Implement fitness function to be initialized with a function instead of deriving

--- src/genalg/algorithm/fitness.hpp
+++ src/genalg/algorithm/fitness.hpp
@@ -1,12 +1,22 @@
 #ifndef GENALG_ALGORITHM_FITNESS_HPP
 #define GENALG_ALGORITHM_FITNESS_HPP
 
+#include <functional>
+
 namespace genalg {
     namespace algorithm {
         template<typename G, typename F>
         class FitnessFunction {
+        private:
+            std::function<F(const G&)> fn_;
+
         public:
-            virtual inline F evaluate(const G& genome) const = 0;
+            explicit FitnessFunction(std::function<F(const G& )> fn)
+                : fn_{fn} {}
+
+            F evaluate(const G& genome) const {
+                return this->fn_(genome);
+            }
         };
     }
 }
