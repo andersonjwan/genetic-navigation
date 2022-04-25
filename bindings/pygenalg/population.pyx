d0fea1201467b2385c1203d8e70881e1a76fe624
Jacob Anderson <andersonjwan@gmail.com>
2022-04-25 02:06:10 -0700

    feat: Implement Population bindings

--- bindings/pygenalg/population.pyx
+++ bindings/pygenalg/population.pyx
@@ -1,15 +1,56 @@
 # cython: language_level = 3
 # distutils: language = c++
 
+from typing import List
+
+from cython.operator cimport dereference
+
+from libcpp cimport bool
+from libcpp.vector cimport vector
+
+from .individualcpp cimport Individual as cppIndividual
 from .populationcpp cimport Population as cppPopulation
 
+from .individual import Individual as pyIndividual
+
+cdef class Individual:
+    cdef cppIndividual[vector[bool], double]* _objcpp
+
 cdef class Population:
-    cdef cppPopulation[double]* _objcpp
+    cdef cppPopulation[cppIndividual[vector[bool], double]]* _objcpp
+
+    _capacity: int
+    _individuals: List[pyIndividual]=[]
 
     def __cinit__(self, capacity: int) -> None:
-        self._objcpp = new cppPopulation[double](capacity)
+        self._capacity = capacity
+        self._objcpp = new cppPopulation[cppIndividual[vector[bool], double]](capacity)
+
         print("cython: cppPopulation allocated...")
 
     def __dealloc__(self) -> None:
         del self._objcpp
         print("cython: cppPopulation deallocated...")
+
+    @property
+    def capacity(self) -> int:
+        return self._objcpp.capacity()
+
+    @property
+    def individuals(self) -> List[pyIndividual]:
+        return self._individuals
+
+    def append(self, individual: pyIndividual) -> None:
+        assert(len(self._individuals) < self._capacity)
+
+        self._objcpp.append(dereference((<Individual>individual)._objcpp))
+        self._individuals.append(individual)
+
+    def best(self) -> pyIndividual:
+        return max(self._individuals)
+
+    def worst(self) -> pyIndividual:
+        return min(self._individuals)
+
+    def __len__(self) -> int:
+        return len(self._individuals)
