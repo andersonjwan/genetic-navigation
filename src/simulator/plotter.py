91324ee8e6c23a26762d73b23719d0f1b4df69b8
Jacob Anderson <andersonjwan@gmail.com>
2022-04-28 17:32:17 -0700

    feat: Add average fitness per generation plot

--- src/simulator/plotter.py
+++ src/simulator/plotter.py
@@ -14,7 +14,54 @@ class Plotter:
     def __init__(self, ga: GeneticAlgorithm) -> None:
         self._ga = ga
 
-    def best(self, show=True, save=False) -> None:
+    def average(self, show=True, save=False) -> None:
+        data = {
+            "Generation": [],
+            "Fitness": [],
+            "StandardDeviation": []
+        }
+
+        for i, population in enumerate(self._ga.generations):
+            fitnesses = [x.fitness for x in population.individuals]
+
+            data["Generation"].append(i + 1)
+            data["Fitness"].append(np.average(fitnesses))
+            data["StandardDeviation"].append(np.std(fitnesses))
+
+        fig = go.Figure([
+            go.Scatter(
+                name="Average",
+                x=data["Generation"],
+                y=data["Fitness"],
+                line=dict(color="rgb(0, 100, 80)"),
+                mode="lines",
+                showlegend=False
+            ),
+            go.Scatter(
+                x=data["Generation"] + data["Generation"][::-1],
+                y=(
+                    (np.array(data["Fitness"]) + np.array(data["StandardDeviation"])).tolist() +
+                    (np.array(data["Fitness"]) + (-1 * np.array(data["StandardDeviation"]))).tolist()[::-1]
+                ),
+                fill="toself",
+                fillcolor="rgba(0, 100, 80, 0.2)",
+                line=dict(color="rgba(255, 255, 255, 0)"),
+                hoverinfo="skip",
+                showlegend=False
+            )
+        ])
+
+        fig.update_layout(
+            title="Average Fitness per Generation",
+            xaxis_title="Generation",
+            yaxis_title="Fitness"
+        )
+
+        fig.write_image(f"img/{self._ga.seed}_average_fitness_per_generation.svg") if save else None
+        fig.show() if show else None
+
+
+    def best(self, show=True, save=False) -> Dict:
         data = {
             "Generation": [],
             "Fitness": []
